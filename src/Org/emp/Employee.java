74215cb570c33aeb3e1a0d610d12fd274ecef3a8
harivarman <harivarman280590@gmail.com>
2021-08-10 17:48:18 +0530

    Git practice

---
+++ src/Org/emp/Employee.java
@@ -0,0 +1,21 @@
+package Org.emp;
+
+public class Employee {
+	private void empID() {
+		System.out.println("Emp ID");
+	}
+	private void empName() {
+		System.out.println("Emp Name");
+
+	}
+	public static void main(String[] args) {
+		
+		Employee E1 = new Employee();
+		
+		E1.empID();
+		E1.empName();
+		
+	}
+	
+	
+}
